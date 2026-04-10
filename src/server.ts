86e27b7994eeab012c3d5156988807351b8298d9
Bernardo <bernardobregeron@gmail.com>
2026-04-10 01:27:50 -0300

    cors update

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,12 @@ import { swaggerSpec } from "./config/swagger.js";
 
 const app = express();
 const PORT = process.env.PORT || 3333;
-const allowedOrigins = ["", process.env.FRONTEND_URL ?? ""].filter(Boolean);
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.FRONTEND_URL ?? "",
+].filter(Boolean);
+
+console.log("Allowed origins:", allowedOrigins);
 
 app.use(
   cors({
